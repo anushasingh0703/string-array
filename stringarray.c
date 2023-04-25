cf139365ed0c10eb14a5c9ce87a43e4e9ee2ee76
anushasingh0703 <122735664+anushasingh0703@users.noreply.github.com>
2023-04-26 03:05:35 +0530

    Add files via upload

---
+++ stringarray.c
@@ -0,0 +1,13 @@
+//Anusha Singh, PRN-21070123019
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+ {  
+  int a;
+	char name[]= "SYMBIOSIS";
+        printf("%s",name);
+	return 0;
+}
